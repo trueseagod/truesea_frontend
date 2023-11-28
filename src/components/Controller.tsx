67de4f96c8da6d621e4515d6a5200b8bf6156d60
ts <trueseagd@gmail.com>
2023-11-28 14:21:50 -0500

    update strings

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -43,8 +43,8 @@ const Controller = () => {
             audio.src = createBlobURL(blob);
 
             // Append to audio
-            const malfurionMessage = { sender: "Malfurion", blobUrl: audio.src };
-            messagesArr.push(malfurionMessage);
+            const AJBotMessage = { sender: "AJBot", blobUrl: audio.src };
+            messagesArr.push(AJBotMessage);
             setMessages(messagesArr);
 
             // Play audio
@@ -72,14 +72,14 @@ const Controller = () => {
                 key={index + audio.sender}
                 className={
                   "flex flex-col " +
-                  (audio.sender == "Malfurion" && "flex items-end")
+                  (audio.sender == "AJBot" && "flex items-end")
                 }
               >
                 {/* Sender */}
                 <div className="mt-4 ">
                   <p
                     className={
-                      audio.sender == "Malfurion"
+                      audio.sender == "AJBot"
                         ? "text-right mr-2 italic text-green-500"
                         : "ml-2 italic text-blue-500"
                     }
@@ -100,7 +100,7 @@ const Controller = () => {
 
           {messages.length == 0 && !isLoading && (
             <div className="text-center font-light italic mt-10">
-              Send Malfurion a message...
+              Send AJBot a message...
             </div>
           )}
 
